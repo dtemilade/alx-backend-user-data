987c8cd733539854ea3591b39e8e420db31dbbf0
dtemilade <dtemilade@gmail.com>
2024-05-24 12:25:41 +0100

    Updating commits for task accomplished

--- 0x02-Session_authentication/main_all.py
+++ 0x02-Session_authentication/main_all.py
@@ -1,13 +1,145 @@
 #!/usr/bin/env python3
-""" Main 100
+""" Main 4
 """
 import base64
-from api.v1.auth.basic_auth import BasicAuth
+from flask import Flask, request
+from api.v1.auth.session_auth import SessionAuth
 from models.user import User
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.auth import Auth
+
 
 """ Create a user test """
-user_email = "bob100@hbtn.io"
-user_clear_pwd = "H0lberton:School:98!"
+user_email = "bobsession@hbtn.io"
+user_clear_pwd = "fake pwd"
+
+user = User()
+user.email = user_email
+user.password = user_clear_pwd
+user.save()
+
+""" Create a session ID """
+sa = SessionAuth()
+session_id = sa.create_session(user.id)
+print("User with ID: {} has a Session ID: {}".format(user.id, session_id))
+
+""" Create a Flask app """
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root_path():
+    """ Root path
+    """
+    request_user = sa.current_user(request)
+    if request_user is None:
+        return "No user found\n"
+    return "User found: {}\n".format(request_user.id)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
+
+auth = Auth()
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root_path():
+    """ Root path
+    """
+    return "Cookie value: {}\n".format(auth.session_cookie(request))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
+
+""" Main 2
+"""
+
+sa = SessionAuth()
+
+user_id_1 = "abcde"
+session_1 = sa.create_session(user_id_1)
+print("{} => {}: {}".format(user_id_1, session_1, sa.user_id_by_session_id))
+
+user_id_2 = "fghij"
+session_2 = sa.create_session(user_id_2)
+print("{} => {}: {}".format(user_id_2, session_2, sa.user_id_by_session_id))
+
+print("---")
+
+tmp_session_id = None
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = 89
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = "doesntexist"
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+print("---")
+
+tmp_session_id = session_1
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = session_2
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+print("---")
+
+session_1_bis = sa.create_session(user_id_1)
+print("{} => {}: {}".format(user_id_1, session_1_bis, sa.user_id_by_session_id))
+
+tmp_user_id = sa.user_id_for_session_id(session_1_bis)
+print("{} => {}".format(session_1_bis, tmp_user_id))
+
+tmp_user_id = sa.user_id_for_session_id(session_1)
+print("{} => {}".format(session_1, tmp_user_id))
+
+
+""" Main 1
+"""
+
+sa = SessionAuth()
+
+print("{}: {}".format(type(sa.user_id_by_session_id), sa.user_id_by_session_id))
+
+user_id = None
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = 89
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "abcde"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "fghij"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "abcde"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+
+""" Main 0
+"""
+
+""" Create a user test """
+user_email = "bob@hbtn.io"
+user_clear_pwd = "H0lbertonSchool98!"
 
 user = User()
 user.email = user_email
@@ -17,91 +149,3 @@ user.save()
 
 basic_clear = "{}:{}".format(user_email, user_clear_pwd)
 print("Basic Base64: {}".format(base64.b64encode(basic_clear.encode('utf-8')).decode("utf-8")))
-
-
-""" Main 6
-"""
-import base64
-from api.v1.auth.basic_auth import BasicAuth
-from models.user import User
-
-""" Create a user test """
-user_email = "bob@hbtn.io"
-user_clear_pwd = "H0lbertonSchool98!"
-user = User()
-user.email = user_email
-user.password = user_clear_pwd
-print("New user: {} / {}".format(user.id, user.display_name()))
-user.save()
-
-basic_clear = "{}:{}".format(user_email, user_clear_pwd)
-print("Basic Base64: {}".format(base64.b64encode(basic_clear.encode('utf-8')).decode("utf-8")))
-
-
-
-""" Main 4
-"""
-from api.v1.auth.basic_auth import BasicAuth
-
-a = BasicAuth()
-
-print(a.extract_user_credentials(None))
-print(a.extract_user_credentials(89))
-print(a.extract_user_credentials("Holberton School"))
-print(a.extract_user_credentials("Holberton:School"))
-print(a.extract_user_credentials("bob@gmail.com:toto1234"))
-
-""" Main 3
-"""
-from api.v1.auth.basic_auth import BasicAuth
-
-a = BasicAuth()
-
-print(a.decode_base64_authorization_header(None))
-print(a.decode_base64_authorization_header(89))
-print(a.decode_base64_authorization_header("Holberton School"))
-print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
-print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
-print(a.decode_base64_authorization_header(
-    a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))
-
-
-""" Main 2
-"""
-from api.v1.auth.basic_auth import BasicAuth
-
-a = BasicAuth()
-
-print(a.extract_base64_authorization_header(None))
-print(a.extract_base64_authorization_header(89))
-print(a.extract_base64_authorization_header("Holberton School"))
-print(a.extract_base64_authorization_header("Basic Holberton"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
-print(a.extract_base64_authorization_header("Basic1234"))
-
-
-""" Main 1
-"""
-from api.v1.auth.auth import Auth
-
-a = Auth()
-
-print(a.require_auth(None, None))
-print(a.require_auth(None, []))
-print(a.require_auth("/api/v1/status/", []))
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
-
-
-""" Main 0
-"""
-from api.v1.auth.auth import Auth
-
-a = Auth()
-
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.authorization_header())
-print(a.current_user())
